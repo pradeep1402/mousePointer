6cd45db5c52ddb6283a4524fb5038dcd84229668
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-03-13 00:32:54 +0530

    added canvasview class

--- script/canvas.js
+++ script/canvas.js
@@ -1,67 +1,41 @@
-const lastMousePos = { x: 0, y: 0 };
+class CanvasView {
+  constructor(target) {
+    this.target = target;
+  }
 
-const drawCircle = ({ clientX, clientY, target }) => {
-  if (!(target.classList.contains('canvas'))) return;
+  toggleMouseDraw = (event, dot) => {
+    if (!(event.key === 'Control')) return;
 
-  const div = document.createElement("div");
-  div.style.top = `${clientY}px`;
-  div.style.left = `${clientX}px`;
-  div.classList.add("dot");
-  target.appendChild(div);
+    if (event.type === 'keydown') {
+      addEventListener("mousemove", this.drawShape(event, dot));
+      return;
+    }
 
-  lastMousePos.x = clientX;
-  lastMousePos.y = clientY;
-};
-
-const drawUsingArrowKeys = (code, canvas) => {
-  const coordinates = canvas.getBoundingClientRect();
-  let newX = lastMousePos.x;
-  let newY = lastMousePos.y;
-  const arrowKeys = {
-    'ArrowUp': () => newY -= 6,
-    'ArrowDown': () => newY += 6,
-    'ArrowLeft': () => newX -= 6,
-    'ArrowRight': () => newX += 6
+    removeEventListener('mousemove', this.drawShape);
   };
 
-  code in arrowKeys && arrowKeys[code]();
+  drawShape = ({ clientX, clientY }, classList = ['dot']) => {
+    const div = document.createElement("div");
+    div.style.top = `${clientY}px`;
+    div.style.left = `${clientX}px`;
+    div.classList.add(...classList);
+    this.target.appendChild(div);
+  };
 
-  if (isInBoundary(newX, newY, coordinates)) {
-    drawCircle({ clientX: newX, clientY: newY, target: canvas });
-  }
-};
+  moveCursorUsingKeys = ({ code }) => {
+    drawUsingArrowKeys(code, this.target);
+  };
 
-const isInBoundary = (newX, newY, { left, right, top, bottom }) => {
-  return newX >= left && newX <= right && newY >= top && newY <= bottom;
-};
-
-const moveCursorUsingKeys = ({ code }) => {
-  const parent = document.querySelector('.canvas');
-  drawUsingArrowKeys(code, parent);
-};
-
-const toggleMouseDraw = (event) => {
-  if (!(event.key === 'Control')) return;
-
-  if (event.type === 'keydown') {
-    addEventListener("mousemove", drawCircle);
-    return;
-  }
-
-  removeEventListener('mousemove', drawCircle);
-};
-
-const clearCanvas = () => {
-  const canvas = document.querySelector('.canvas');
-  const dots = document.querySelectorAll('.dot');
-  dots.forEach(child => canvas.removeChild(child));
-};
+  clearCanvas = () => {
+    const dots = document.querySelectorAll('dot');
+    dots.forEach(dot => dot.removeChild());
+  };
+}
 
 const main = () => {
-  document.addEventListener("keydown", toggleMouseDraw);
-  document.addEventListener("keyup", toggleMouseDraw);
-  document.addEventListener("keydown", moveCursorUsingKeys);
-  document.querySelector('#reset-btn').addEventListener('click', clearCanvas);
+  const parent = document.querySelector('canvas');
+  const canvas = new CanvasView(parent);
+
 };
 
 window.onload = main;
